337d8f33b87d626928da153c8ebe1b99aa22f8c2
Mariam ElGhobary <M.ElGhobary@aucegypt.edu>
2024-07-04 03:19:06 +0300

    Implemented verification and home navigation with heggi

--- app/src/main/java/com/example/fasa7ni/Home.java
+++ app/src/main/java/com/example/fasa7ni/Home.java
@@ -1,8 +1,13 @@
 package com.example.fasa7ni;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +15,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Home extends AppCompatActivity
+public class Home extends AppCompatActivity implements View.OnClickListener
 {
+    private String email;
+    private ImageButton List;
+    private ImageButton Events;
+    private ImageButton Friends;
+    private ImageButton Recommender;
+    private ImageButton Profile;
+
     @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,5 +31,89 @@ public class Home extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        Start();
+
     }
+
+    private void Start()
+    {
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null)
+        {
+            email = bundle.getString("Email");
+        }
+
+        List = findViewById(R.id.small_list_btn);
+        Events = findViewById(R.id.small_event_btn);
+        Friends = findViewById(R.id.small_friends_btn);
+        Recommender = findViewById(R.id.small_recommender_btn);
+        Profile = findViewById(R.id.profile_btn);
+
+        List.setOnClickListener(this);
+        Events.setOnClickListener(this);
+        Friends.setOnClickListener(this);
+        Recommender.setOnClickListener(this);
+        Profile.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==List.getId())
+            Go_List();
+
+        else if (v.getId()==Events.getId())
+            Go_Events();
+
+        else if (v.getId()==Friends.getId())
+            Go_Friends();
+
+        else if (v.getId()==Recommender.getId())
+            Go_Recommender();
+
+        else if (v.getId()==Profile.getId())
+            Go_Profile();
+
+        else
+            return;
+
+    }
+
+    private void Go_List()
+    {
+        Intent H_L = new Intent(Home.this, List.class);
+        H_L.putExtra("Email", email);
+        startActivity(H_L);
+    }
+
+    private void Go_Events()
+    {
+        Intent H_E = new Intent(Home.this, Events.class);
+        H_E.putExtra("Email", email);
+        startActivity(H_E);
+    }
+
+    private void Go_Friends()
+    {
+        Intent H_F = new Intent(Home.this, Friends.class);
+        H_F.putExtra("Email", email);
+        startActivity(H_F);
+    }
+
+    private void Go_Recommender()
+    {
+        Intent H_R = new Intent(Home.this, Recommender.class);
+        H_R.putExtra("Email", email);
+        startActivity(H_R);
+    }
+
+    private void Go_Profile()
+    {
+        Intent H_P = new Intent(Home.this, Profile.class);
+        H_P.putExtra("Email", email);
+        startActivity(H_P);
+    }
+
+
 }
--- app/src/main/java/com/example/fasa7ni/Verification.java
+++ app/src/main/java/com/example/fasa7ni/Verification.java
@@ -1,8 +1,13 @@
 package com.example.fasa7ni;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +15,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Verification extends AppCompatActivity
+public class Verification extends AppCompatActivity  implements View.OnClickListener
 {
+    private Button Verify;
+    private  String email;
+    private String phone;
+    private EditText User_Code;
+    private boolean verified;
     @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,5 +29,54 @@ public class Verification extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.verification);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        Start();
+    }
+
+    private void Start()
+    {
+
+        Verify = findViewById(R.id.verify_btn);
+        Verify.setOnClickListener(this);
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null)
+        {
+            email = bundle.getString("Email");
+            phone = bundle.getString("Phone");
+        }
+        //request code from API
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==Verify.getId()) {
+            verified = Confirm_Code();
+
+            if(verified)
+                Go_Home();
+            else
+                Toast.makeText(Verification.this, "Incorrect Code", Toast.LENGTH_SHORT).show();
+        }
+        else
+            return;
+    }
+
+    private boolean Confirm_Code()
+    {
+        String code;
+        code= User_Code.getText().toString();
+
+        //Check with requested code
+
+        return true;
+    }
+
+    private void Go_Home()
+    {
+        Intent V_H = new Intent(Verification.this, Home.class);
+        V_H.putExtra("Email", email);
+        startActivity(V_H);
     }
 }
